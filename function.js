53bb9c4ba2e25af9d2b3f6f0b93859bbd183c457
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-23 17:14:09 +0100

    Create function.js

---
+++ function.js
@@ -0,0 +1,15 @@
+//initialize when scene starts
+//diff not update every frame (index)
+$this.move=function(){
+  $this.moveY(10);
+}
+$this.move=function(){
+  $this.moveY(-10);
+}
+$this.move=function(){
+  $this.moveX(10);
+}
+$this.move=function(){
+  $this.moveX(-10);
+}
+//Note: BINARY IS HARD
