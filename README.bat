cb146461ed2f2bf42dcfadb028238269a4b4d455
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-23 08:27:55 +0100

    Update README.bat

--- README.bat
+++ README.bat
@@ -1,5 +1,5 @@
 //add an empty grid as a background for pac-man
 //add in a pac man and four ghosts, name the ghosts 
 //Blinky, Pinky, Icy and Clyde
-//When the pac-man image is selected, add a pac man image when its' mouth is open and closed
+//When the pac-man image is selected, add a pac man image when its' mouth is open and closed, also add a few more designs when pac man is facing up, down, left and right
 //then merge the designs as one
