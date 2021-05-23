4018668da006481b6acb5db6c75d1ac5245c89a9
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-23 18:11:12 +0100

    Update README.bat

--- README.bat
+++ README.bat
@@ -3,3 +3,4 @@
 //Blinky, Pinky, Icy and Clyde
 //When the pac-man image is selected, add a pac man image when its' mouth is open and closed, also add a few more designs when pac man is facing up (frame index is 0), down (frame index is 2), left (frame index is 1) and right (frame index is 3)
 //then merge the designs as one
+//also add pellets
